7289509caf5c4ff2df702d5ae8a8164978e164ad
Lucy <lucy70381@kimo.com>
2023-01-27 00:36:06 +0800

    feat: gitignore add dist folder

--- Week2 - integration with MongoDB/dist/src/services/headers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var headers = {
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, OPTIONS, DELETE',
-    'Content-Type': 'application/json',
-};
-exports.default = headers;
