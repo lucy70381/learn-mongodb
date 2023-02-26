58cfaec55e4c1fbccbc2fc2e5dcf5f7ba0dbf245
Lucy <lucy70381@kimo.com>
2023-02-27 03:50:16 +0800

    feat: fill in the answer in the comments

--- 13.express_generater/routes/users.js
+++ 13.express_generater/routes/users.js
@@ -25,12 +25,20 @@ router.post('/', async (req, res, next) => {
     let { nickName, gender, avatar } = data;
     if (!nickName || !gender) {
       // 回傳失敗
+      res.status(400).json({ status: 'fail', message: '使用者資料未填寫完整' });
     } else {
       // 新增至 User model
+      const newUser = await User.create({
+        nickName,
+        gender,
+        avatar,
+      });
       // 回傳成功
+      res.status(200).json({ status: 'success', data: newUser });
     }
   } catch (error) {
     // 回傳失敗
+    res.status(400).json({ status: 'fail', message: error.message });
   }
 });
 
@@ -38,27 +46,36 @@ router.post('/', async (req, res, next) => {
 router.patch('/:id', async (req, res, next) => {
   try {
     // 取得 id
-
+    const { id } = req.params;
     const data = req.body;
     let { nickName, gender, avatar } = data;
     if (!nickName || !gender) {
       // 回傳失敗 "使用者資料未填寫完整"
+      res
+        .status(400)
+        .json(
+          { status: 'fail', message: '使用者資料未填寫完整' },
+          { new: true }
+        );
     } else {
       const editContent = {
         nickName,
         gender,
       };
       // 找出此筆 id 並編輯資料
-      // const editUser =
+      const editUser = await User.findByIdAndUpdate(id, editContent);
 
       if (editUser) {
         // 回傳成功
+        res.status(200).json({ status: 'success', data: editUser });
       } else {
         // 回傳失敗 "id 不存在"
+        res.status(400).json({ status: 'fail', message: 'id 不存在' });
       }
     }
   } catch (error) {
     // 回傳失敗
+    res.status(400).json({ status: 'fail', message: error.message });
   }
 });
 
