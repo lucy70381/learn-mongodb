d43fce2be52c67320b3c51a426b3871077bf2b58
Lucy <lucy70381@kimo.com>
2023-02-27 01:10:40 +0800

    feat: add mongoose, cors

--- 13.express_generater/app.js
+++ 13.express_generater/app.js
@@ -2,12 +2,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://127.0.0.1:27017/learn-mongo');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
