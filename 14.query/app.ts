0d8c9276c28d31eafc403330b664268f050b5d75
Lucy <lucy70381@kimo.com>
2023-02-27 17:11:16 +0800

    feat: add req.query practice

---
+++ 14.query/app.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+// localhost:3000/products?category=music&page=1
+app.get('/products', function (req, res) {
+  // 取出參數
+  /* 請在此填寫答案*/
+  res.status(200).json({
+    status: 'success',
+    data: {
+      /* 請在此填寫答案*/
+    },
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
