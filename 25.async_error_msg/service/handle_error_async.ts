fd840fc9c5c6e562656a2d87bab23f63ee1746b2
Lucy <lucy70381@kimo.com>
2023-10-28 17:22:59 +0800

    feat: add handleErrorAsync func

---
+++ 25.async_error_msg/service/handle_error_async.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
+
+const handleErrorAsync =
+  (fn: (req: Request, res: Response, next: NextFunction) => Promise<void>) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch(next);
+  };
+export default handleErrorAsync;
